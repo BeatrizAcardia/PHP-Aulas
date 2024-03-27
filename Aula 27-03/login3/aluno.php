93e17a0ffdce1ef87566a2dd9f805ed213424a56
mordokx <matt.ag@hotmail.com>
2024-03-27 09:22:02 -0300

    Aula 27/03/2024

---
+++ Aula 27-03/login3/aluno.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema Acadêmico</title>
+</head>
+<body>
+    <a href="">Controle de frequência</a>
+    <a href="">Controle de notas</a>
+    <hr>
+
+    <h1>Seja bem-vindo, aluno <?=$_GET["login"]; ?> </h1>
+</body>
+</html>
---
+++ Aula 27-03/login3/index2.php
@@ -0,0 +1,48 @@
+<?php
+    if ($_SERVER["REQUEST_METHOD"] === 'GET'){
+        $pagina = "";
+    } else if ($_SERVER["REQUEST_METHOD"] === 'POST'){
+        $pagina = "";
+
+        $login = $_POST["login"];
+        $senha = $_POST["senha"];
+
+        if ((trim($login) != " ") && (trim($senha) != "")){
+            if (($login == "João") && ($senha == "1234")){
+                header('Location: professor.php?login='.$login);
+            } else if (($login == "Julio") && ($senha == "5678")){
+                header('Location: aluno.php?login='.$login);
+            } else {
+                $msg = "Login ou senha inválido(s)!";
+                header('Location: erro.php?msg='.$msg);
+            }
+        } else {
+            echo "Informe seu login e sua senha";
+        }
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema Acadêmico</title>
+</head>
+<body>
+    <h1 style="text-align: center;">Sistema Acadêmico</h1>
+    <hr>
+    <form method="post">
+        Login:<br>
+        <input type="text" name="login" id="login" required>
+
+        <br><br>
+
+        Senha:<br>
+        <input type="password" name="senha" id="senha" required>
+        <br><br>
+        <input type="submit" value="Realizar login">
+        <hr>
+    </form>
+</body>
+</html>
